1f3c5063e6ba52b3ecd3297ba4d0d0573672d797
Michael Bestas <mikeioannina@gmail.com>
2013-03-08 05:36:16 +0200

    msm7x30-common: some cleanup
    * Removed useless & incorrect PRODUCT_MANUFACTURER definition
    * Added & fixed some comments to the source files
    * Deleted unused file recovery_ui.c
    * Clean up recovery_keys.c according to bootable/recovery/
    * Move gsm feature xml to mogami-common & zeus
    * Remove custom RIL since it's merged in official cm10.1
    * Revert init.sh modifications, no need for being different from official cm10
    * Remove akm8973 header, we don't have any device using this
    * Update releasetools
    
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: I1e94132ca9491a34e8d5c817d639765401d60bd0

--- combinedroot/init.sh
+++ combinedroot/init.sh
@@ -29,38 +29,13 @@ busybox mount -t proc proc /proc
 busybox mount -t sysfs sysfs /sys
 busybox mount -t yaffs2 ${BOOTREC_CACHE} /cache
 
-# fixing CPU clocks to avoid issues in recovery
-busybox echo 1024000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-busybox echo 122000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-
-# trigger lime green LED & button-backlight
-busybox echo 25 > ${BOOTREC_LED_RED}
-busybox echo 255 > ${BOOTREC_LED_GREEN}
-busybox echo 0 > ${BOOTREC_LED_BLUE}
-busybox echo 255 > ${BOOTREC_LED_BUTTONS}
-busybox echo 50 > ${BOOTREC_VIBRATOR}
-busybox cat ${BOOTREC_EVENT} > /dev/keycheck&
-busybox sleep 1
-
-# trigger pink LED & button-backlight
-busybox echo 100 > ${BOOTREC_LED_RED}
-busybox echo 35 > ${BOOTREC_LED_GREEN}
-busybox echo 50 > ${BOOTREC_LED_BLUE}
-busybox echo 255 > ${BOOTREC_LED_BUTTONS}
-busybox echo 50 > ${BOOTREC_VIBRATOR}
-busybox cat ${BOOTREC_EVENT} > /dev/keycheck&
-busybox sleep 1
-
-# trigger aqua blue LED & button-backlight
-busybox echo 0 > ${BOOTREC_LED_RED}
-busybox echo 100 > ${BOOTREC_LED_GREEN}
+# trigger amber LED & button-backlight
+busybox echo 255 > ${BOOTREC_LED_RED}
+busybox echo 0 > ${BOOTREC_LED_GREEN}
 busybox echo 255 > ${BOOTREC_LED_BLUE}
 busybox echo 255 > ${BOOTREC_LED_BUTTONS}
 
-# trigger vibrator
-busybox echo 200 > ${BOOTREC_VIBRATOR}
-
-# keycheckblue
+# keycheck
 busybox cat ${BOOTREC_EVENT} > /dev/keycheck&
 busybox sleep 3
 
@@ -70,26 +45,26 @@ load_image=/sbin/ramdisk.cpio
 # boot decision
 if [ -s /dev/keycheck -o -e /cache/recovery/boot ]
 then
-busybox echo 'RECOVERY BOOT' >>boot.txt
-busybox rm -fr /cache/recovery/boot
-# trigger gold led
-busybox echo 90 > ${BOOTREC_LED_RED}
-busybox echo 255 > ${BOOTREC_LED_GREEN}
-busybox echo 0 > ${BOOTREC_LED_BLUE}
-busybox echo 0 > ${BOOTREC_LED_BUTTONS}
-# framebuffer fix
-busybox echo 0 > /sys/module/msm_fb/parameters/align_buffer
-# recovery ramdisk
-load_image=/sbin/ramdisk-recovery.cpio
+	busybox echo 'RECOVERY BOOT' >>boot.txt
+	busybox rm -fr /cache/recovery/boot
+	# trigger blue led
+	busybox echo 0 > ${BOOTREC_LED_RED}
+	busybox echo 0 > ${BOOTREC_LED_GREEN}
+	busybox echo 255 > ${BOOTREC_LED_BLUE}
+	busybox echo 0 > ${BOOTREC_LED_BUTTONS}
+	# framebuffer fix
+	busybox echo 0 > /sys/module/msm_fb/parameters/align_buffer
+	# recovery ramdisk
+	load_image=/sbin/ramdisk-recovery.cpio
 else
-busybox echo 'ANDROID BOOT' >>boot.txt
-# poweroff LED & button-backlight
-busybox echo 0 > ${BOOTREC_LED_RED}
-busybox echo 0 > ${BOOTREC_LED_GREEN}
-busybox echo 0 > ${BOOTREC_LED_BLUE}
-busybox echo 0 > ${BOOTREC_LED_BUTTONS}
-# framebuffer fix
-busybox echo 1 > /sys/module/msm_fb/parameters/align_buffer
+	busybox echo 'ANDROID BOOT' >>boot.txt
+	# poweroff LED & button-backlight
+	busybox echo 0 > ${BOOTREC_LED_RED}
+	busybox echo 0 > ${BOOTREC_LED_GREEN}
+	busybox echo 0 > ${BOOTREC_LED_BLUE}
+	busybox echo 0 > ${BOOTREC_LED_BUTTONS}
+	# framebuffer fix
+	busybox echo 1 > /sys/module/msm_fb/parameters/align_buffer
 fi
 
 # kill the keycheck process
